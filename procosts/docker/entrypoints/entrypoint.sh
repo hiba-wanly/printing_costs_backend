f0ec0e25c7f61689ca2ddfe8a81fd2ea00fbf5b3
Rayad Kubaisi <rayad.kubaisi@gmail.com>
2024-07-25 15:43:09 +0000

    Update entrypoint.sh

--- procosts/docker/entrypoints/entrypoint.sh
+++ procosts/docker/entrypoints/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# python manage.py makemigrations
-# python manage.py migrate
-# python manage.py test
+python manage.py makemigrations
+python manage.py migrate
+python manage.py test
 
 # exec "$@"
